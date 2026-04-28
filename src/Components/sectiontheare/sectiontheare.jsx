5a0fd68985bb9305e71b2215d3ea62904edcb254
Farhod Gofurov <fg325609@gmail.com>
2026-04-28 19:53:03 +0500

    aslfhsdj

--- src/Components/sectiontheare/sectiontheare.jsx
+++ src/Components/sectiontheare/sectiontheare.jsx
@@ -4,20 +4,19 @@ import "./sectiontheare.scss"; // Fayl nomi ham to'g'ri bo'lishi kerak
 function SectionThree() {
     return (
         <section className="section-three">
-            <div className="container">
-                <div className="section-wrapper">
-                    <div className="left">
-                        <h1>Guides by Thousand Sunny</h1>
-                        <p>Packed with tips and advice from our on-the-ground experts...</p>
-                        <a href="#!" className="download-btn">Download</a> 
-                        {/* "#" o'rniga "#!" ishlatish sahifa sakrab ketishini oldini oladi */}
-                    </div>
-                    <div className="right">
-                        <img src="/img/sectionthree.png" alt="City Guides App" />
-                    </div>
-                </div>
-            </div>
-        </section>
+  <div className="container">
+    <div className="section-wrapper">
+      <div className="left">
+        <h1>Guides by Thousand Sunny</h1>
+        <p>Packed with tips and advice from our on-the-ground experts...</p>
+        <a href="#!" className="download-btn">Download</a>
+      </div>
+      <div className="right">
+        <img src="img/sectiontheare.png" alt="City Guides App" />
+      </div>
+    </div>
+  </div>
+</section>
     );
 }
 
