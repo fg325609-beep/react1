7a1b96434913e59ba4e982d44db841d6641b3e53
Farhod Gofurov <fg325609@gmail.com>
2026-04-28 11:13:52 +0500

    jldfsh

--- src/Components/sectiontheare/sectiontheare.jsx
+++ src/Components/sectiontheare/sectiontheare.jsx
@@ -1,25 +1,24 @@
 import "./sectiontheare.scss";
 import React from 'react';
-function Sectiontheare(){
-    return(
-        <>
-        <section>
-            <div className="container">
-                <div className="sectionthare">
-                    <div className="left">
 
-                    <h1>Guides by Thousand Sunny</h1>
-                    <p>Packed with tips and advice from our on-the-ground experts, our city guides app (iOS and Android) is the ultimate resource before and during a trip.</p>
-                    <a href="">Download</a>
+function Sectiontheare() {
+    return (
+        <section className="section-three"> {/* Klas nomini aniqroq berdik */}
+            <div className="container">
+                <div className="section-wrapper"> {/* Iloji boricha 'theare' kabi xato so'zlardan qoching */}
+                    <div className="left">
+                        <h1>Guides by Thousand Sunny</h1>
+                        <p>Packed with tips and advice from our on-the-ground experts, our city guides app (iOS and Android) is the ultimate resource before and during a trip.</p>
+                        <a href="#" className="download-btn">Download</a> {/* href bo'sh bo'lmasin */}
                     </div>
                     <div className="right">
-                        <img src="img/sectiontheare.png" alt="" />
+                        {/* Rasm borligiga va yo'li to'g'riligiga ishonch hosil qiling */}
+                        <img src="/img/sectiontheare.png" alt="City Guides App" />
                     </div>
-
                 </div>
             </div>
         </section>
-        </>
-    )
+    );
 }
+
 export default Sectiontheare;
