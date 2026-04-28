de1f4ff398358aa1c0b1f125abf42e3faeddb3aa
Farhod Gofurov <fg325609@gmail.com>
2026-04-28 20:14:37 +0500

    djdjjddj

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Components/header/Header";
 import Hero from "./Components/hero/Hero";
 import SectionOne from "./Components/sectionone/Sectionone";
 import SectionTwo from "./Components/sectiontwo/Sectiontwo";
-import SectionThree from "./Components/sectiontheare/sectiontheare"; // Nomni to'g'riladik
+import SectionThree from "./Components/sectiontheare/sectiontheare";  
 import Sectionfoo from "./Components/sectionfoo/sectionfoo";
 
 function App() {
--- src/Components/sectionfoo/sectionfoo.jsx
+++ src/Components/sectionfoo/sectionfoo.jsx
@@ -4,7 +4,20 @@ import React from 'react';
 function Sectionfoo(){
     return(
         <>
-        
+        <section>
+            <div className="container">
+                <div className="sectionfoo">
+                    <div className="title">
+                        <h1>Testimonials</h1>
+                    </div>
+                    <div className="sectionfoo-left">
+                      <h1>“Quisque in lacus a urna fermentum euismod. Integer mi nibh, dapibus ac scelerisque eu, facilisis quis purus. Morbi blandit sit amet turpis nec”</h1>
+                      <p>Edward Newgate</p>
+                      <a href="">Founder Circle</a>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
