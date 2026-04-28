9e0a7652d6523948e8e67b488364103cad1db9c2
Farhod Gofurov <fg325609@gmail.com>
2026-04-28 19:39:30 +0500

    asdljhfdkasjhf

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Header from "./Components/header/Header";
 import Hero from "./Components/hero/Hero";
-import Sectionone from "./Components/sectionone/Sectionone";
-import Sectiontwo from "./Components/sectiontwo/Sectiontwo"; // Agar fayl nomi Sectiontwo bo'lsa, S ni katta qiling
-import Sectiontheare from "./Components/sectiontheare/Sectiontheare"; // "Sectiontheare" - bosh harfga e'tibor bering
+import SectionOne from "./Components/sectionone/Sectionone";
+import SectionTwo from "./Components/sectiontwo/Sectiontwo";
+import SectionThree from "./Components/sectionthree/SectionThree"; // Nomni to'g'riladik
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <Sectionone />
-      <Sectiontwo />
-      <Sectiontheare />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
     </>
   );
 }
