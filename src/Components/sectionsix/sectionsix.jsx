babda3984a46d58710339dc1366750c59f646596
Farhod Gofurov <fg325609@gmail.com>
2026-04-29 08:58:01 +0500

    adflkjdsghfdf'

--- src/Components/sectionsix/sectionsix.jsx
+++ src/Components/sectionsix/sectionsix.jsx
@@ -4,43 +4,43 @@ import React from 'react';
 function Sectionsix(){
     return(
         <>
-        <footer>
+       <footer>
     <div className="container">
-        {/* Logo va ma'lumot qismi */}
+        {/* 1-ustun: Logo va ma'lumot */}
+        <div className="footer-info"> 
+            <img src="img/logo.png" alt="Thousand Sunny Logo" />
+            <p>Plan and book your perfect trip with expert advice, travel tips destination information from us</p>
+            <p className="copyright">©2020 Thousand Sunny. All rights reserved</p>
+        </div>
+
+        {/* 2-ustun: Destinations */}
         <ul>
-            <li><img src="img/logo.png" alt="Logo" /></li>
-            <li><p>Plan and book your perfect trip with expert advice, travel tips destination information from us</p></li>
-            <li><p>©2020 Thousand Sunny. All rights reserved</p></li>
+            <li><strong>Destinations</strong></li>
+            <li><a href="#africa">Africa</a></li>
+            <li><a href="#antarctica">Antarctica</a></li>
+            <li><a href="#asia">Asia</a></li>
+            <li><a href="#europe">Europe</a></li>
+            <li><a href="#america">America</a></li>
         </ul>
 
-        {/* Destinations qismi */}
+        {/* 3-ustun: Shop */}
         <ul>
-            <li><a href="#">Destinations</a></li>
-            <li><a href="#">Africa</a></li>
-            <li><a href="#">Antarctica</a></li>
-            <li><a href="#">Asia</a></li>
-            <li><a href="#">Europe</a></li>
-            <li><a href="#">America</a></li>
+            <li><strong>Shop</strong></li>
+            <li><a href="#guides">Destination Guides</a></li>
+            <li><a href="#gifts">Pictorial & Gifts</a></li>
+            <li><a href="#offers">Special Offers</a></li>
+            <li><a href="#delivery">Delivery Times</a></li>
+            <li><a href="#faqs">FAQs</a></li>
         </ul>
 
-        {/* Shop qismi */}
+        {/* 4-ustun: Interests */}
         <ul>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">Destination Guides</a></li>
-            <li><a href="#">Pictorial & Gifts</a></li>
-            <li><a href="#">Special Offers</a></li>
-            <li><a href="#">Delivery Times</a></li>
-            <li><a href="#">FAQs</a></li>
-        </ul>
-
-        {/* Interests qismi */}
-        <ul>
-            <li><a href="#">Interests</a></li>
-            <li><a href="#">Adventure Travel</a></li>
-            <li><a href="#">Art And Culture</a></li>
-            <li><a href="#">Wildlife And Nature</a></li>
-            <li><a href="#">Family Holidays</a></li>
-            <li><a href="#">Food And Drink</a></li>
+            <li><strong>Interests</strong></li>
+            <li><a href="#adventure">Adventure Travel</a></li>
+            <li><a href="#art">Art And Culture</a></li>
+            <li><a href="#wildlife">Wildlife And Nature</a></li>
+            <li><a href="#family">Family Holidays</a></li>
+            <li><a href="#food">Food And Drink</a></li>
         </ul>
     </div>
 </footer>
