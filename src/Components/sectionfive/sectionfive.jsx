90da21e79313b8b634c1f771315cedfa1d8598c5
Farhod Gofurov <fg325609@gmail.com>
2026-04-29 08:39:53 +0500

    jkjjj

--- src/Components/sectionfive/sectionfive.jsx
+++ src/Components/sectionfive/sectionfive.jsx
@@ -4,39 +4,38 @@ import React from 'react';
 function Sectionfive(){
     return(
         <>
-        <section className="five">
-            <div className="container">
-                <div className="section-five">
-                    <div className="cards">
-                        <div className="card">
-                            <img src="img/five.png" alt="" />
-                            <h1>The many benefits of  taking a healing holiday</h1>
-                            <p>‘Helaing holidays’ are on the rise tohelp maximise your health and happines...</p>
-                            <a href="">Read more</a>
-                        </div>
-                        <div className="card">
-                            <img src="img/five.png" alt="" />
-                            <h1>The many benefits of  taking a healing holiday</h1>
-                            <p>‘Helaing holidays’ are on the rise tohelp maximise your health and happines...</p>
-                            <a href="">Read more</a>
-                        </div>
-                        <div className="card">
-                            <img src="img/five.png" alt="" />
-                            <h1>The many benefits of  taking a healing holiday</h1>
-                            <p>‘Helaing holidays’ are on the rise tohelp maximise your health and happines...</p>
-                            <a href="">Read more</a>
-                        </div>
-                        <div className="card">
-                            <img src="img/five.png" alt="" />
-                            <h1>The many benefits of  taking a healing holiday</h1>
-                            <p>‘Helaing holidays’ are on the rise tohelp maximise your health and happines...</p>
-                            <a href="">Read more</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
+       <section className="five">
+  <div className="container">
+    <div className="section-five">
+      <div className="cards">
+        <div className="card">
+          <img src="img/five.png" alt="Healing holiday" />
+          <h1>The many benefits of taking a healing holiday</h1>
+          <p>‘Healing holidays’ are on the rise to help maximise your health and happiness...</p>
+          <a href="#">Read more</a>
+        </div>
+        <div className="card">
+          <img src="img/five.png" alt="Healing holiday" />
+          <h1>The many benefits of taking a healing holiday</h1>
+          <p>‘Healing holidays’ are on the rise to help maximise your health and happiness...</p>
+          <a href="#">Read more</a>
+        </div>
+        <div className="card">
+          <img src="img/five.png" alt="Healing holiday" />
+          <h1>The many benefits of taking a healing holiday</h1>
+          <p>‘Healing holidays’ are on the rise to help maximise your health and happiness...</p>
+          <a href="#">Read more</a>
+        </div>
+        <div className="card">
+          <img src="img/five.png" alt="Healing holiday" />
+          <h1>The many benefits of taking a healing holiday</h1>
+          <p>‘Healing holidays’ are on the rise to help maximise your health and happiness...</p>
+          <a href="#">Read more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
         </>
     )
 
