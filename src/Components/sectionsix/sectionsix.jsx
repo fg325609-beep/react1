696f02376052e8f94039da03d5aa8f761023ae19
Farhod Gofurov <fg325609@gmail.com>
2026-04-29 08:48:51 +0500

    fdjhfjfj

--- src/Components/sectionsix/sectionsix.jsx
+++ src/Components/sectionsix/sectionsix.jsx
@@ -4,6 +4,40 @@ import React from 'react';
 function Sectionsix(){
     return(
         <>
+        <footer>
+            <div className="container">
+                <ul>
+
+                <img src="img/logo.png" alt="" />
+                <p>Plan and book your perfect trip with expert advice, travel tips destinationinformation from us</p>
+                <p>©2020 Thousand Sunny. All rights reserved</p>
+                </ul>
+                <ul>
+                    <li><a href="">Destinations</a></li>
+                    <li><a href="">Africa</a></li>
+                    <li><a href="">Antarctica</a></li>
+                    <li><a href="">Asia</a></li>
+                    <li><a href="">Europe</a></li>
+                    <li><a href="">America</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Shop</a></li>
+                    <li><a href="">Destination Guides</a></li>
+                    <li><a href="">Pictorial & Gifts</a></li>
+                    <li><a href="">Special Offers</a></li>
+                    <li><a href="">Delivery Times   </a></li>
+                    <li><a href="">FAQs</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">Interests</a></li>
+                    <li><a href="">Adventure Travel</a></li>
+                    <li><a href="">Art And Culture</a></li>
+                    <li><a href="">Wildlife And Nature</a></li>
+                    <li><a href="">Family Holidays</a></li>
+                    <li><a href="">Food And Drink</a></li>
+                </ul>
+            </div>
+        </footer>
         </>
     )
 }
